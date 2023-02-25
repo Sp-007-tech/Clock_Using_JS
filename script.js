7eb524bed593aed699c9a331d8bf39e56017752c
Shailes Pethkar <104692252+Sp-007-tech@users.noreply.github.com>
2023-02-25 14:02:42 +0530

    Uploaded !

---
+++ script.js
@@ -0,0 +1,29 @@
+function displaytime()
+{
+    var datetime = new Date();
+    var hrs = datetime.getHours();
+    var min = datetime.getMinutes();
+    var sec = datetime.getSeconds();
+    var meridian = document.getElementById('meridian');
+
+    if(hrs >=12)
+    {
+        meridian.innerHTML = "PM";
+    }
+    else
+    {
+        meridian.innerHTML = "AM";
+    }
+
+    if(hrs > 12)
+    {
+        hrs = hrs - 12;
+    }
+
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+    
+}
+
+setInterval(displaytime, 10);
